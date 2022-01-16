5708778c5c86199db81eb902fe1b91a78469f7eb
Daniel Rill <92930703+danielrill@users.noreply.github.com>
2022-01-16 19:46:23 +0100

    Delete Versuch5/Code directory

--- Versuch5/Code/aufgabe2.py
+++
@@ -1,5 +0,0 @@
-import redlab as rl
-print("------- einzelne Werte -------------------------")
-print("16 Bit Value: " + str(rl.cbAIn(0, 0, 1)))
-print("Voltage Value: " + str(rl.cbVIn(0, 0, 1)))
-print("------- Messreihe -------------------------")
